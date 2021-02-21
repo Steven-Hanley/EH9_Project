acc589d3dc9331db3be7703022af3c959b377216
Steven-Hanley <hanley193@gmail.com>
2021-02-21 18:47:19 +0000

    Added a potential fix for dictionary check

--- API/Prototyping/Dictionary/index.php
+++ API/Prototyping/Dictionary/index.php
@@ -22,55 +22,57 @@
 </body>
 
 <?php
-        
+
     function checkDict(){
 	    if(isset($_POST['dictionaryCheck'])){
             $start = microtime(true);
 		    $password = ($_POST['password']);
             $found = searchDict($password);
-            
+
             $timeTaken = microtime(true) - $start;
-            if($found == true){
+            if($found != -1){
                 echo "password found in wordlist in $timeTaken secs";
             }else{
                 echo "password not found in wordlist in $timeTaken secs";
             }
 	    }	
     }
- 
+
     function searchDict($password){
         $file = file_get_contents("rockyou.txt");
         $found = BoyerMoore($file, $password);
         return $found;
     }
 
-    function BoyerMoore($text, $pattern) {
-        $patlen = strlen($pattern);
-        $textlen = strlen($text);
-        $table = makeCharTable($pattern);
-    
-        for ($i=$patlen-1; $i < $textlen;) { 
-            $t = $i;
-            for ($j=$patlen-1; $pattern[$j]==$text[$i]; $j--,$i--) { 
-                if($j == 0) return $i;
-            }
-            $i = $t;
-            if(array_key_exists($text[$i], $table)){
-                return true;
-            }else{
-                return false;
-            }  
+function BoyerMoore($text, $pattern) {
+    $patlen = strlen($pattern);
+    $textlen = strlen($text);
+    $table = makeCharTable($pattern);
+
+    for ($i=$patlen-1; $i < $textlen;) { 
+        $t = $i;
+        for ($j=$patlen-1; $pattern[$j]==$text[$i]; $j--,$i--) { 
+            if($j == 0) return $i;
         }
+        $i = $t;
+        if(array_key_exists($text[$i], $table))
+
+            $i = $i + max($table[$text[$i]], 1);
+            
+        else
+            $i += $patlen;
     }
-    
-    function makeCharTable($string) {
-        $len = strlen($string);
-        $table = array();
-        for ($i=0; $i < $len; $i++) { 
-            $table[$string[$i]] = $len - $i - 1; 
-        }
-        return $table;
+    return -1;
+}
+
+function makeCharTable($string) {
+    $len = strlen($string);
+    $table = array();
+    for ($i=0; $i < $len; $i++) { 
+        $table[$string[$i]] = $len - $i - 1; 
     }
+    return $table;
+}
 
 
 ?>
