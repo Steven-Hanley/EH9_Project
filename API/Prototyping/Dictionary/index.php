c040adb97cc778566207759b861258ae96607638
Steven-Hanley <hanley193@gmail.com>
2021-02-22 16:30:07 +0000

    Added a few comments rest will be added later on

--- API/Prototyping/Dictionary/index.php
+++ API/Prototyping/Dictionary/index.php
@@ -25,11 +25,18 @@
 
     function checkDict(){
 	    if(isset($_POST['dictionaryCheck'])){
+
+            //take current time for the starting position
             $start = microtime(true);
+
 		    $password = ($_POST['password']);
+
             $found = searchDict($password);
 
+            //gets time taken by subtracting current time from start time
             $timeTaken = microtime(true) - $start;
+
+
             if($found != -1){
                 echo "password found in wordlist in $timeTaken secs";
             }else{
@@ -39,41 +46,48 @@
     }
 
     function searchDict($password){
+        //file = gets file contents of the rockyou.txt
         $file = file_get_contents("rockyou.txt");
+
+        //begins boyermoore function
         $found = BoyerMoore($file, $password);
+
+        //returns found variable
         return $found;
     }
 
-function BoyerMoore($text, $pattern) {
-    $patlen = strlen($pattern);
-    $textlen = strlen($text);
-    $table = makeCharTable($pattern);
+    function BoyerMoore($text, $pattern) {
+        //gets the length of the pattern you are trying to find
+        $patlen = strlen($pattern);
+        //gets the total string length of rock you
+        $textlen = strlen($text);
+        //runs the make character table function
+        $table = makeCharTable($pattern);
 
-    for ($i=$patlen-1; $i < $textlen;) { 
-        $t = $i;
-        for ($j=$patlen-1; $pattern[$j]==$text[$i]; $j--,$i--) { 
-            if($j == 0) return $i;
-        }
-        $i = $t;
-        if(array_key_exists($text[$i], $table))
+        for ($i=$patlen-1; $i < $textlen;) { 
+            $t = $i;
+            for ($j=$patlen-1; $pattern[$j]==$text[$i]; $j--,$i--) { 
+                if($j == 0) return $i;
+            }
+            $i = $t;
+            if(array_key_exists($text[$i], $table))
 
-            $i = $i + max($table[$text[$i]], 1);
+                $i = $i + max($table[$text[$i]], 1);
             
-        else
-            $i += $patlen;
+            else
+                $i += $patlen;
+            }
+        return -1;
     }
-    return -1;
-}
 
-function makeCharTable($string) {
-    $len = strlen($string);
-    $table = array();
-    for ($i=0; $i < $len; $i++) { 
-        $table[$string[$i]] = $len - $i - 1; 
+    function makeCharTable($string) {
+        $len = strlen($string);
+        $table = array();
+        for ($i=0; $i < $len; $i++) { 
+            $table[$string[$i]] = $len - $i - 1; 
+        }
+        return $table;
     }
-    return $table;
-}
-
 
 ?>
 
