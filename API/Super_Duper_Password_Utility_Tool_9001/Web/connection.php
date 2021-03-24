d0ff75e44db12901d7892f3e035d841ac0afbade
Tom G <1800028@abertay.ac.uk>
2021-03-24 20:58:59 +0000

    Login Feature

--- API/Super_Duper_Password_Utility_Tool_9001/Web/connection.php
+++ API/Super_Duper_Password_Utility_Tool_9001/Web/connection.php
@@ -4,8 +4,10 @@
     $username = "***REMOVED***";
     $passwd = "***REMOVED***";
     
+    // Create connection
     $conn = mysql_connect($host, $username, $passwd, $dbase);
 
+    // Check connection
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     } else {
--- API/Super_Duper_Password_Utility_Tool_9001/Web/login_connection.php
+++ API/Super_Duper_Password_Utility_Tool_9001/Web/login_connection.php
@@ -1,5 +1,48 @@
 <?php
+
+    session_start();
     require_once 'connection.php';
+    $response = array();
     
+    if(isset($_GET['apicall'])) {
+
+        switch($_GET['apicall']) {
+           
+            case 'login';           
+            $username = $POST['username'];
+            $password = password_hash($POST['password']);
+            $stmt = $con->prepare('SELECT id, username FROM Accounts WHERE username = ? AND password = ?');
+            $stmt->bindParam(1, $_POST['username']);
+            $stmt->execute();
+            $stmt->store_result();
+
+            if($stmt->num_rows > 0) {
+
+                $stmt->bind_result($id, $username);
+				$stmt->fetch();
+                $user = array(
+                    'id'=>$id,
+                    'username'=>$username,
+                );
+
+                $response['error'] = false;
+                $response['message'] = 'Login successful';
+                $response['user'] = $user;
+            } else {
+                $response['error'] = false;
+                $response['message'] = 'Invalid username or password';
+            }
+        break;
+            case 'signup':
+                break;
+        default:
+                $response['error'] = true;
+                $response['message'] = 'Invalid Operation';
+        }
+    } else {
+        $response['error'] = true;
+        $response['message'] = 'Invalid API Call';
+    }
+
 
 ?>
