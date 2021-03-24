2f0cec16ea29f8ce1bfa2c822a43f08ab1bc7e08
Steven-Hanley <hanley193@gmail.com>
2021-03-24 15:05:11 +0000

    new project with bottom nav

---
+++ EH9project/app/src/main/java/com/eh9/eh9project/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.eh9.eh9project.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
