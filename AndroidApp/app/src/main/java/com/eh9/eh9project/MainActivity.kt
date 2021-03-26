1114579e329c29852b2fb88e7b3149e748c3c13c
Steven-Hanley <hanley193@gmail.com>
2021-03-26 16:59:55 +0000

    Started implementing design for the app theme buttons will be fixed later on. seems to be an issue with night theme at the minute where it doesnt use all the colours provided cos that would make too much sense

--- AndroidApp/app/src/main/java/com/eh9/eh9project/MainActivity.kt
+++ AndroidApp/app/src/main/java/com/eh9/eh9project/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.eh9.eh9project
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.button.MaterialButtonToggleGroup
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +23,14 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        // Please work or your mum gay
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+                R.id.navigation_home, R.id.navigation_account, R.id.navigation_settings))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
+
+
+
     }
 }
