209f005385d239bb3d6289c65ed67ee27a6dbb69
Tom G <1800028@abertay.ac.uk>
2021-04-05 15:37:03 +0100

    Added Test Radar Graph

--- AndroidApp/app/src/main/java/com/eh9/eh9project/RadarTest.kt
+++ AndroidApp/app/src/main/java/com/eh9/eh9project/RadarTest.kt
@@ -1,4 +1,215 @@
 package com.eh9.eh9project
 
-class RadarTest {
+import android.graphics.Color
+import android.graphics.Color.WHITE
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.github.mikephil.charting.charts.RadarChart
+import com.github.mikephil.charting.data.RadarData
+import com.github.mikephil.charting.data.RadarDataSet
+import com.github.mikephil.charting.data.RadarEntry
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.interfaces.datasets.IRadarDataSet
+import java.util.ArrayList
+
+
+
+
+class RadarTest: AppCompatActivity() {
+
+    private var MAX = 12f                       //max value
+    private var  MIN = 1f                       //min value
+    var NB_QUALITIES = 5                        //nb qualities for Radar Chart
+    lateinit var chart: RadarChart
+    lateinit var xAxis: XAxis
+    lateinit var yAxis: YAxis
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.radarmain)
+        chart = findViewById(R.id.chart)
+
+        //configure radar graph
+        chart.setBackgroundColor(Color.rgb(60, 65, 82))
+        chart.description.isEnabled = false
+        chart.webLineWidth(1f)
+        //useful to export graph
+        chart.webColor = WHITE
+        chart.webLineWidth = 1f
+        chart.webColorInner = WHITE
+        chart.webAlpha = 100
+
+        setData()
+
+        //animate the chart
+        chart.animateXY(1400, 1400, Easing.EaseInOutQuad)
+
+        //define axis
+        xAxis  = chart.xAxis
+        xAxis.textSize(9f)
+        xAxis.yOffset(0)
+        xAxis.xOffset(0)
+        xAxis.valueFormatter (IndexAxisValueFormatter {
+
+            val qualities: Array<String> = arrayOf("Length", "Capitals", "Consecutive", "Complexity", "Repeating", "Numbers", "Lowercase")
+
+            fun getFormattedValue(value:Float, axis:AxisBase): String {
+                return qualities[value.toInt() % qualities.size]
+            }
+
+        })
+        xAxis.textColor = WHITE
+
+        yAxis = chart.yAxis
+        yAxis.setLabelCount(NB_QUALITIES, false)
+        yAxis.textSize = 9f
+        yAxis.axisMaximum = MIN
+        yAxis.axisMaximum = MAX
+        yAxis.setDrawLabels(false)
+
+        //configure legend for radar graph
+        val l = chart.legend
+        l.textSize = 15f
+        l.verticalAlignment(Legend.LegendVerticalAlignment.TOP)
+        l.horizontalAlignment(Legend.LegendHorizontalAlignment.CENTER)
+        l.orientation(Legend.LegendOrientation.HORIZONTAL)
+        l.setDrawInside(false)
+        l.xEntrySpace = 7f
+        l.yEntrySpace = 5f
+        l.textColor = WHITE
+    }
+
+    private fun IndexAxisValueFormatter(strings: () -> Unit): Any {
+        return true
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.radargraph, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.refreshValues -> {
+                setData()
+                chart.invalidate()
+            }
+            R.id.toggleValues -> {
+                for (set in chart.data.dataSets)
+                {
+                    set.setDrawValues(!set.isDrawValuesEnabled)
+                }
+                chart.invalidate()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setData() {
+        val employee1:ArrayList<RadarEntry> = ArrayList()
+        val employee2:ArrayList<RadarEntry> = ArrayList()
+
+        NB_QUALITIES.forEach { ->
+            val val1 = (Math.random()).toInt() * MAX +MIN
+            employee1.add(RadarEntry(val1))
+
+            val val2 = (Math.random()).toInt() * MAX +MIN
+            employee2.add(RadarEntry(val2))
+        }
+
+        //create two radar data sets objects with these data
+        val set1 = RadarDataSet(employee1, "Employee A")
+        set1.color = Color.RED
+        set1.fillColor = Color.RED
+        set1.setDrawFilled(true)
+        set1.lineWidth = 2f
+        set1.setDrawHighlightIndicators(false)
+        set1.isDrawHighlightCircleEnabled
+
+        val set2 = RadarDataSet(employee1, "Employee B")
+        set1.color = Color.RED
+        set1.fillColor = Color.RED
+        set1.setDrawFilled(true)
+        set1.lineWidth = 2f
+        set1.setDrawHighlightIndicators(false)
+        set1.isDrawHighlightCircleEnabled
+
+        val sets:ArrayList<IRadarDataSet> = ArrayList()
+        sets.add(set1)
+        sets.add(set2)
+
+        // create Radar Data object which will be added to radar chart
+        val data = RadarData(sets)
+        data.setValueTextSize(8f)
+        data.setDrawValues(false)
+        data.setValueTextColor(WHITE)
+
+        chart.data = data
+        chart.invalidate()
+
+    }
+
+
 }
+
+private fun Int.forEach(any: Any) {
+
+}
+
+class string {
+
+}
+
+
+private fun Any.orientation(horizontal: Legend.LegendOrientation) {
+
+}
+
+
+
+private fun Any.horizontalAlignment(center: Legend.LegendHorizontalAlignment) {
+
+}
+
+private fun Any.verticalAlignment(top: Legend.LegendVerticalAlignment) {
+
+}
+
+
+
+private fun XAxis.textColor(white: Int) {
+
+}
+
+private fun XAxis.valueFormatter(indexAxisValueFormatter: Any) {
+
+}
+
+private fun XAxis.xOffset(i: Int) {
+
+
+}
+
+private fun XAxis.yOffset(i: Int) {
+
+    return
+
+}
+
+private fun XAxis.textSize(fl: Float) {
+
+}
+
+private fun RadarChart.webLineWidth(fl: Float) {
+    return
+}
+
+
