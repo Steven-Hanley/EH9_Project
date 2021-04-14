508a98a2ad16cb260cfa8871bf845cc5d258dbcb
Steven-Hanley <hanley193@gmail.com>
2021-04-14 19:44:53 +0100

    Added ability to pop back to the home page from the Radar Page

--- AndroidApp/app/src/main/java/com/eh9/eh9project/classes/user.kt
+++ AndroidApp/app/src/main/java/com/eh9/eh9project/classes/user.kt
@@ -1,6 +1,6 @@
 package com.eh9.eh9project.classes
 
-//User Class for Account Features Password isnt saved just needed to make the conversion for json work
+//User Class for Account Features Password isn't saved just needed to make the conversion for json work
 data class user(
         var user_id: Int? = null,
         var username: String? = null,
